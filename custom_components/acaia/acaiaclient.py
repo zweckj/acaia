d4e8c90fa59dae4ee61c972c951655eb99b7a6b1
Josef Zweck <24647999+zweckj@users.noreply.github.com>
2023-12-08 05:35:50 +0100

    some cleanup

--- custom_components/acaia/acaiaclient.py
+++ custom_components/acaia/acaiaclient.py
@@ -1,7 +1,6 @@
 """Acaia Scale Client for Home Assistant."""
 from collections.abc import Awaitable, Callable
 import logging
-import time
 
 from bleak import BleakGATTCharacteristic
 from pyacaia_async import AcaiaScale
@@ -28,19 +27,14 @@ class AcaiaClient(AcaiaScale):
         notify_callback: Callable[[], None] | None = None,
     ) -> None:
         """Initialize the client."""
-        name = entry.data[CONF_NAME]
-        mac = entry.data[CONF_MAC]
-        is_new_style_scale = entry.data.get(CONF_IS_NEW_STYLE_SCALE, True)
 
-        self._last_action_timestamp: float | None = None
         self.hass: HomeAssistant = hass
         self.entry: ConfigEntry = entry
-        self._name: str = name
-        self._device_available: bool = False
+        self._name: str = entry.data[CONF_NAME]
 
         super().__init__(
-            mac=mac,
-            is_new_style_scale=is_new_style_scale,
+            mac=entry.data[CONF_MAC],
+            is_new_style_scale=entry.data.get(CONF_IS_NEW_STYLE_SCALE, True),
             notify_callback=notify_callback,
         )
 
@@ -68,7 +62,6 @@ class AcaiaClient(AcaiaScale):
                 self.new_client_from_ble_device(ble_device)
                 await super().connect(callback=callback)
 
-            self._last_action_timestamp = time.time()
         except (AcaiaDeviceNotFound, AcaiaError) as ex:
             _LOGGER.warning(
                 "Couldn't connect to device %s with MAC %s", self.name, self.mac
@@ -76,7 +69,7 @@ class AcaiaClient(AcaiaScale):
             _LOGGER.debug("Full error: %s", str(ex))
 
     def _setup_tasks(self) -> None:
-        """Setup background tasks"""
+        """Setup background tasks."""
 
         if not self._heartbeat_task or self._heartbeat_task.done():
             self._heartbeat_task = self.entry.async_create_background_task(
@@ -102,32 +95,22 @@ class AcaiaClient(AcaiaScale):
             _LOGGER.debug("Update coordinator: No bluetooth scanner available")
             return
 
-        self._device_available = bluetooth.async_address_present(
+        device_available = bluetooth.async_address_present(
             self.hass, self.mac, connectable=True
         )
 
-        if not self._connected and self._device_available:
-            if self._last_disconnect_time and self._last_disconnect_time > (
-                time.time() - 15
-            ):
-                _LOGGER.debug(
-                    "Acaia Client: Device with MAC %s disconnected less than 15 seconds ago, skipping connect",
-                    self.mac,
-                )
-                return
-            _LOGGER.debug("Acaia Client: Connecting")
+        if device_available:
             await self.connect()
+            # send auth to get the battery level and units
+            await self.auth()
+            await self.send_weight_notification_request()
 
-        elif not self._device_available:
+        else:
             self._connected = False
-            self.timer_running = False
+            self._timer_running = False
             _LOGGER.debug(
                 "Acaia Client: Device with MAC %s not available",
                 self.mac,
             )
             if self._notify_callback:
                 self._notify_callback()
-        else:
-            # send auth to get the battery level and units
-            await self.auth()
-            await self.send_weight_notification_request()
