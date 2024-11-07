6d9b910d5d1f4fec201154927e43410189989bfe
Josef Zweck <24647999+zweckj@users.noreply.github.com>
2024-11-07 09:44:29 +0100

    fix flow

--- custom_components/acaia/config_flow.py
+++ custom_components/acaia/config_flow.py
@@ -4,24 +4,18 @@ from typing import Any
 
 import voluptuous as vol
 
-from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
+from homeassistant.config_entries import (
+    SOURCE_USER,
+    ConfigFlow,
+    ConfigFlowResult,
+)
 from homeassistant.const import CONF_MAC, CONF_NAME
 
 from .const import CONF_IS_NEW_STYLE_SCALE, DOMAIN
 
-DEFAULT_SCHEMA = vol.Schema(
-    {
-        vol.Required(CONF_MAC): str,
-    }
-)
-
-SCALE_VERSION_SCHEMA = vol.Schema(
-    {vol.Optional(CONF_IS_NEW_STYLE_SCALE, default=True): bool}
-)
-
 
 class AcaiaConfigFlow(ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for Acaia."""
+    """Handle a config flow for acaia."""
 
     VERSION = 2
 
@@ -35,8 +29,9 @@ class AcaiaConfigFlow(ConfigFlow, domain=DOMAIN):
         """Handle a flow initialized by the user."""
 
         if user_input is not None:
-            await self.async_set_unique_id(user_input[CONF_MAC])
-            self._abort_if_unique_id_configured()
+            if self.source == SOURCE_USER:
+                await self.async_set_unique_id(user_input[CONF_MAC])
+                self._abort_if_unique_id_configured()
             return self.async_create_entry(
                 title="acaia",
                 data={**self._discovered, **user_input},
@@ -44,29 +39,37 @@ class AcaiaConfigFlow(ConfigFlow, domain=DOMAIN):
 
         return self.async_show_form(
             step_id="user",
-            data_schema=(
-                DEFAULT_SCHEMA.extend(SCALE_VERSION_SCHEMA)
-                if not self._discovered
-                else SCALE_VERSION_SCHEMA
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_MAC): str,
+                    vol.Optional(CONF_IS_NEW_STYLE_SCALE, default=True): bool,
+                },
             ),
         )
 
     async def async_step_bluetooth(self, discovery_info) -> ConfigFlowResult:
         """Handle a discovered Bluetooth device."""
+
         self._discovered[CONF_MAC] = discovery_info.address
         self._discovered[CONF_NAME] = discovery_info.name
 
         await self.async_set_unique_id(discovery_info.address)
         self._abort_if_unique_id_configured()
 
-        return await self.async_step_user()
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {vol.Optional(CONF_IS_NEW_STYLE_SCALE, default=True): bool}
+            ),
+        )
 
     async def async_step_reconfigure(
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
         """Perform reconfiguration of the config entry."""
+        reconfigure_entry = self._get_reconfigure_entry()
+
         if not user_input:
-            reconfigure_entry = self._get_reconfigure_entry()
             return self.async_show_form(
                 step_id="reconfigure",
                 data_schema=vol.Schema(
@@ -83,4 +86,4 @@ class AcaiaConfigFlow(ConfigFlow, domain=DOMAIN):
                 ),
             )
 
-        return await self.async_step_user(user_input)
+        return await self.async_step_user({**reconfigure_entry.data, **user_input})
